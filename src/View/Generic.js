35915e37360f417510a5784ae282280e910cf690
AbduvoiotovAsadbek <abduvoitovasadbek3@gmail.com>
2021-08-27 14:12:47 +0500

    Service Card coplited

--- src/View/Generic.js
+++ src/View/Generic.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Body from "../components/MainBody";
-
+import ServiceCard from "../components/ServiceCard";
 export const Generic = (props) => {
   return (
     <div>
       <Body />
+      <ServiceCard />
     </div>
   );
 };
