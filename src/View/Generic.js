34222638a42b64ce66e257bda66a22d28cfefbd1
AbduvoiotovAsadbek <abduvoitovasadbek3@gmail.com>
2021-08-28 10:52:57 +0500

    Price crated in branch Master

--- src/View/Generic.js
+++ src/View/Generic.js
@@ -2,12 +2,14 @@ import React from "react";
 import Body from "../components/MainBody";
 import Service from "../components/Service";
 import DoctorList from "../components/DoctorList";
+import Price from "../components/PriceList";
 export const Generic = (props) => {
   return (
     <div>
       <Body />
       <Service />
       <DoctorList />
+      <Price />
     </div>
   );
 };
---
+++ src/components/PriceList/index.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { price } from "../../mock/price";
+import Fade from "react-reveal/Fade";
+
+import {
+  Header,
+  Container,
+  Text,
+  Box,
+  Title,
+  Button,
+  Wrapper,
+  TextWrapper,
+  MediumText,
+  ButtonWrapper,
+} from "./style";
+export default function Price() {
+  return (
+    <Header>
+      <Wrapper>
+        <Fade duration={800} left cascade>
+          <Text>Price List</Text>
+        </Fade>
+        <Container>
+          <Box left>
+            <TextWrapper>
+              <Fade top cascade>
+                <MediumText>Name of Service</MediumText>
+                <MediumText>Price of Service</MediumText>
+              </Fade>
+            </TextWrapper>
+            {price.map(({ title, price }) => (
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <Fade delay={700} top cascade>
+                  <Title>{title}</Title>
+                </Fade>
+                <Fade top delay={700} cascade>
+                  <Title>{price} / hr</Title>
+                </Fade>
+              </div>
+            ))}
+          </Box>
+          <Box>
+            <TextWrapper>
+              <Fade top cascade>
+                <MediumText>Name of Service</MediumText>
+                <MediumText>Price of Service</MediumText>
+              </Fade>
+            </TextWrapper>
+            {price.map(({ title, price }) => (
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <Fade top delay={700} cascade>
+                  <Title>{title}</Title>
+                </Fade>
+                <Fade top delay={700} cascade>
+                  <Title>{price} / hr</Title>
+                </Fade>
+              </div>
+            ))}
+          </Box>
+        </Container>
+        <ButtonWrapper>
+          <Fade top cascade>
+            <Button>All Price</Button>
+          </Fade>
+        </ButtonWrapper>
+      </Wrapper>
+    </Header>
+  );
+}
