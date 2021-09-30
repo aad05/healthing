c618b7f91bac120cd099cf048455341656e91c78
AbduvoiotovAsadbek <abduvoitovasadbek3@gmail.com>
2021-09-30 12:43:25 +0500

    Service Page doneeeee Congratullations

--- src/page/Service/index.jsx
+++ src/page/Service/index.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import ServiceBuddy from "../../components/ServiceBuddy";
 import ServiceAbout from "../../components/ServiceAbout";
+import Price from "../../components/PriceList";
+import ServiceList from "../../components/Service";
+import Recomadation from "../../components/Recomadation";
+import Maps from "../../components/Map";
+import Footer from "../../components/newfooter";
 export const Service = () => {
   return (
     <div>
       <ServiceBuddy />
       <ServiceAbout />
+      <Price />
+      <ServiceList />
+      <Recomadation />
+      <Maps />
+      <Footer />
     </div>
   );
 };
